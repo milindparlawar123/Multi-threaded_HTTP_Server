07b795b7c1c2482737b25fc60132ff8c12e77f34
milindparlawar123 <milindparlawar123@gmail.com>
2020-09-27 10:57:28 -0400

    multi threaded http web server coding and UT completed

--- src/Server.java
+++ src/Server.java
@@ -7,42 +7,64 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ServerSocket serverSocket=null;
-		Socket socket=null;
-		InputStream inputStream=null;
-		OutputStream outputStream=null;
-		try {
-		    serverSocket = new ServerSocket(8080);
-		    System.out.println("LISTENING");
-		    socket= serverSocket.accept();
-			
-			 inputStream =  socket.getInputStream();
-			 outputStream= socket.getOutputStream();
-			 String res="<html> <body><h1>how are you</h1><body> </html>";
-			 String req = "HTTP/1.0 200 OK\n"
-				
-				
-				+"Content-Length: " + (res.length()) + "\n\n";
 
-				String header = req  + res;
-			 outputStream.write(header.getBytes());
-		} catch (IOException e) {
+		ServerThread serverThread = new ServerThread();
+		serverThread.start();
+
+	}
+
+}
+
+class ServerThread extends Thread {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
+		try {
+			serverSocket = new ServerSocket(8080);
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			
+			e1.printStackTrace();
+		}
+		while (true) {
+
 			try {
-				socket.close();
-				serverSocket.close();
-				inputStream.close();
-				outputStream.close();
+
+				System.out.println("LISTENING");
+				socket = serverSocket.accept();
+
+				inputStream = socket.getInputStream();
+				outputStream = socket.getOutputStream();
 				
+				
+				String res = "<html> <body><h1>how are you, doing good?</h1><body> </html>";
+				String req = "HTTP/1.0 200 OK\n"
+
+						+ "Content-Length: " + (res.length()) + "\n\n";
+
+				String header = req + res;
+				outputStream.write(header.getBytes());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+
+				try {
+					socket.close();
+					// serverSocket.close();
+					inputStream.close();
+					outputStream.close();
+
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("closed");
 			}
-			System.out.println("closed");
 		}
 
 	}
